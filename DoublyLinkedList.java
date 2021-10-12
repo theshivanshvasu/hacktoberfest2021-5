2547f594004f4954f9c8f6441c5c3549ec9e801f
samarakoonsach <91709729+samarakoonsach@users.noreply.github.com>
2021-10-12 12:33:03 +0530

    Create Doubly Linked List
    Example Java program to indicate Doubly Linked List data structure

---
+++ DoublyLinkedList.java
@@ -0,0 +1,44 @@
+
+public class DoublyLinkedList<int> {
+
+    // Front / head node
+    private ListNode<int> front;
+
+    // Helper, keeping track of size.
+    private int size;
+
+    /**
+     * Constructing empty list.
+     */
+    public DoublyLinkedList() {
+        front = null;
+        size = modificationCount = 0;
+    }
+
+    public void addFront(int x) {
+    }
+
+    public void addEnd(int x) {
+
+    }
+
+    public void addBefore(int x, int y) {
+
+    }
+
+    public void addAfter(int x, int y) {
+
+    }
+
+    public void remove(int x) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+}
